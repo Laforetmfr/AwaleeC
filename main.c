834afc3eb6ad0c6e7068d42f79138c7fb53bc237
Laforetmfr <laforet.mathis6@gmail.com>
2024-12-17 14:43:29 +0100

    Update main.c

--- main.c
+++ main.c
@@ -5,9 +5,9 @@
 #include <SDL_ttf.h>
 
 #define TAILLE_PLATEAU 6
-#define INITIAL_GRANDES 4
-#define LARGEUR_CASE 130
-#define HAUTEUR_CASE 130
+#define INITIAL_GRAINES 4
+#define LARGEUR_CASE 150
+#define HAUTEUR_CASE 150
 #define ESPACE 10
 
 int plateau[2][TAILLE_PLATEAU];
@@ -24,8 +24,8 @@ void afficher_scores(SDL_Renderer* renderer, TTF_Font* font);
 // Fonction pour initialiser le plateau
 void initialiser_jeu() {
     for (int i = 0; i < TAILLE_PLATEAU; i++) {
-        plateau[0][i] = INITIAL_GRANDES;
-        plateau[1][i] = INITIAL_GRANDES;
+        plateau[0][i] = INITIAL_GRAINES;
+        plateau[1][i] = INITIAL_GRAINES;
     }
 }
 
@@ -72,35 +72,47 @@ int get_case_from_click(int x, int y, int joueur) {
     return -1;
 }
 
-// Fonction pour semer les graines
 void semer_graines(int joueur, int case_depart) {
-    int graines = plateau[joueur][case_depart];
-    plateau[joueur][case_depart] = 0;
-    int i = case_depart + 1;
-    int adversaire = 1 - joueur;
+    int graines = plateau[joueur][case_depart]; // Nombre de graines à semer
+    plateau[joueur][case_depart] = 0;          // Vide la case de départ
 
+    int i = case_depart;    // Position actuelle
+    int camp = joueur;      // Camp actuel (0 ou 1)
+
+    // Distribution des graines
     while (graines > 0) {
-        if (i >= TAILLE_PLATEAU) {
-            joueur = adversaire;
-            i = 0;
+        if (camp == 0) { // Camp du bas (joueur 0)
+            i++; // Avance vers la droite
+            if (i >= TAILLE_PLATEAU) { // Si on dépasse la dernière case
+                camp = 1; // Passe en haut (camp adverse)
+                i = TAILLE_PLATEAU - 1; // Commence à droite
+            }
+        } else { // Camp du haut (joueur 1)
+            i--; // Avance vers la gauche
+            if (i < 0) { // Si on dépasse la première case
+                camp = 0; // Retour en bas (camp joueur 0)
+                i = 0; // Commence à gauche
+            }
         }
-        if (!(joueur == adversaire && i == case_depart)) { // On saute la case d'origine
-            plateau[joueur][i]++;
+
+        if (!(camp == joueur && i == case_depart)) { // Ne pas ressemer dans la case d'origine
+            plateau[camp][i]++;
             graines--;
         }
-        i++;
     }
 
-    // Vérifie les captures
-    i--;
-    if (joueur != adversaire) { // Captures seulement dans le camp adverse
-        while (i >= 0 && plateau[adversaire][i] >= 2 && plateau[adversaire][i] <= 3) {
-            score[1 - adversaire] += plateau[adversaire][i];
-            plateau[adversaire][i] = 0;
-            i--;
+    // Capture des graines si on termine dans le camp adverse
+    int adversaire = 1 - joueur;
+    if (camp == adversaire) {
+        while (i >= 0 && i < TAILLE_PLATEAU && (plateau[adversaire][i] == 2 || plateau[adversaire][i] == 3)) {
+            score[joueur] += plateau[adversaire][i]; // Ajouter les graines au score
+            plateau[adversaire][i] = 0;             // Vider la case capturée
+            if (camp == 1) i--; // Avance vers la gauche si camp du haut
+            else i++; // Avance vers la droite si camp du bas
         }
     }
 }
+
 
 // Fonction pour détecter la fin de la partie
 int fin_de_partie() {
@@ -116,17 +128,29 @@ int fin_de_partie() {
 void afficher_scores(SDL_Renderer* renderer, TTF_Font* font) {
     char score_text[50];
     snprintf(score_text, sizeof(score_text), "Joueur 1: %d - Joueur 2: %d", score[0], score[1]);
-    // Affichage du texte (nécessite SDL_ttf)
+
     SDL_Color textColor = { 0, 0, 0, 255 };
     SDL_Surface* surfaceMessage = TTF_RenderText_Solid(font, score_text, textColor);
     SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
 
-    SDL_Rect Message_rect; // Création d'un rectangle pour afficher le texte
-    Message_rect.x = 10;
-    Message_rect.y = 10;
-    Message_rect.w = surfaceMessage->w;
-    Message_rect.h = surfaceMessage->h;
+    SDL_Rect Message_rect = {10, 10, surfaceMessage->w, surfaceMessage->h};
+    SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
+    SDL_FreeSurface(surfaceMessage);
+    SDL_DestroyTexture(Message);
+}
 
+// Fonction pour afficher un message de fin de partie
+void afficher_message_fin(SDL_Renderer* renderer, TTF_Font* font) {
+    char message_fin[50];
+    if (score[0] > score[1]) snprintf(message_fin, sizeof(message_fin), "Joueur 1 gagne avec %d points!", score[0]);
+    else if (score[1] > score[0]) snprintf(message_fin, sizeof(message_fin), "Joueur 2 gagne avec %d points!", score[1]);
+    else snprintf(message_fin, sizeof(message_fin), "Match nul!");
+
+    SDL_Color textColor = { 255, 0, 0, 255 };
+    SDL_Surface* surfaceMessage = TTF_RenderText_Solid(font, message_fin, textColor);
+    SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+
+    SDL_Rect Message_rect = {300, 300, surfaceMessage->w, surfaceMessage->h};
     SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
     SDL_FreeSurface(surfaceMessage);
     SDL_DestroyTexture(Message);
@@ -142,11 +166,10 @@ int main(int argc, char* argv[]) {
     initialiser_jeu();
     int quit = 0, joueur = 0;
 
-    // Charger la police
     TTF_Font* font = TTF_OpenFont("C:/Windows/Fonts/arial.ttf", 24);
-    if (font == NULL) {
-        printf("Erreur lors du chargement de la police : %s\n", TTF_GetError());
-        return 1;  // Arrêt du programme en cas d'erreur
+    if (!font) {
+        printf("Erreur chargement police : %s\n", TTF_GetError());
+        return 1;
     }
 
     while (!quit) {
@@ -158,8 +181,13 @@ int main(int argc, char* argv[]) {
                 int case_choisie = get_case_from_click(e.button.x, e.button.y, joueur);
                 if (case_choisie >= 0 && plateau[joueur][case_choisie] > 0) {
                     semer_graines(joueur, case_choisie);
-                    if (fin_de_partie()) quit = 1;
-                    joueur = 1 - joueur;
+                    if (fin_de_partie()) {
+                        afficher_message_fin(renderer, font);  // Appel à la fonction afficher_message_fin
+                        SDL_RenderPresent(renderer);  // Assure que le message est bien affiché
+                        SDL_Delay(5000);  // Attendre 5 secondes avant de quitter
+                        quit = 1;  // Met fin à la boucle après la fin de partie
+                    }
+                    joueur = 1 - joueur;  // Change de joueur
                 }
             }
         }
@@ -167,11 +195,10 @@ int main(int argc, char* argv[]) {
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderClear(renderer);
         dessiner_plateau(renderer);
-        afficher_scores(renderer, font); // Afficher les scores avec la police chargée
+        afficher_scores(renderer, font);
         SDL_RenderPresent(renderer);
     }
 
-    // Libérer les ressources
     TTF_CloseFont(font);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
@@ -179,3 +206,4 @@ int main(int argc, char* argv[]) {
     TTF_Quit();
     return 0;
 }
+
